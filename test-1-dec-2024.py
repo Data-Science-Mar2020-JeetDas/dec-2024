455dc60dcfc0b8bae0771649157af9e4002f08cf
Jeet Das <53969369+2019jeetdas@users.noreply.github.com>
2024-12-21 11:42:04 +0530

    Create test-1-dec-2024.py

---
+++ test-1-dec-2024.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import random
+import pandas
+
+
+df = pd.read_csv("C:/Users/Jeet Das/Desktop/health.csv",encoding="utf-8")
+
+print("\n----------------------- output data :---------------------\n")
+print("Project - 16 : Funding For Health In Eleventh Plan - Core And Broad Health Components ");
+print("\n------------------------------------------------------------\n")
+
+
+# Question – A : get row and column numbers 
+
+print('---------------------------------------------')
+print("Dimension of the data frame = ",df.shape)
+print('---------------------------------------------')
+
+
+----------------------- output data :---------------------
+
+Project - 16 : Funding For Health In Eleventh Plan - Core And Broad Health Components 
+
+------------------------------------------------------------
+
+---------------------------------------------
+Dimension of the data frame =  (6, 9)
+---------------------------------------------
